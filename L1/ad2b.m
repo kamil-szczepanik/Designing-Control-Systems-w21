3237b9b0333e908e2fad761f41d46e945c8f8a55
HKozubek <01149402@pw.edu.pl>
2021-10-28 14:14:22 +0200

    Charakterystyka statyczna wykres

---
+++ L1/ad2b.m
@@ -0,0 +1,20 @@
+load("u50.mat");
+i50 = i-1;
+u50 = measurements;
+load("u75.mat");
+i75 = i-1;
+u75 = measurements;
+load("u100.mat");
+i100 = i-1;
+u100 = measurements;
+plot([u50,u75,u100]);
+grid on
+x = [25,50,75, 100]';
+y = [29.31,u50(i50), u75(i75), u100(i100)]';
+X = [x.^0, x];
+b = (X'*X)\X'*y;
+ym = X*b;
+scatter([25,50,75, 100], [29.31,u50(i50), u75(i75), u100(i100)])
+hold on
+fplot(@(u)20.97 + 0.3322*u);
+hold off
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Pomiar temperatury: 29,31
 
 ## AD2 
 Pomiar 1: plik (u30.mat)[L1/u30.mat]
+20.97 + 0.3322*u
