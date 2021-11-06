9f20d584b88f0a538b061b75156b42d1be209a98
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-11-06 11:18:34 +0100

    ad3 revert

--- L1/ad3.m
+++ L1/ad3.m
@@ -5,20 +5,16 @@ load("u75.mat");
 S = measurements;
 S = (S(1:i)-S(1))/(75 - 25);
 T = zeros(10,3);
-params = zeros(10,3);
 loss = zeros(10,1);
 for k = 0:1:9
     Td = k;
     options = optimoptions(@fminunc,'MaxIterations',1000,'MaxFunctionEvaluations',2000);
-    [params(k+1,:),loss(k+1)] = fminunc(@f,[80,40,0.3],options);
+    [T(k+1,:),loss(k+1)] = fminunc(@f,[80,40,0.3],options);
 end
 [~,ind] = min(loss);
-params = params(ind,:);
+T = T(ind,:);
 Td = ind-1;
-T1 = params(1);
-T2 = params(2);
-K = params(3);
-obj = double_inertial(K,Td,T1,T2);
+obj = double_inertial(T(3),Td,T(1),T(2));
 [~, ~,y] = systemSim(@(y,y_zad)1, obj, 1, 1, i);
 disp(norm(S-y));
 
@@ -26,13 +22,10 @@ hold on
 stairs(S);
 stairs(y);
 hold off
-function loss = f(params)
+function loss = f(T)
     global S Td
     i = 317;
-    T1 = params(1);
-    T2 = params(2);
-    K = params(3);
-	obj = double_inertial(K,Td,T1,T2);
+	obj = double_inertial(T(3),Td,T(1),T(2));
     [~, ~,y] = systemSim(@(y,y_zad)1, obj, 1, 1, i);
     loss = norm(S-y);
 end
