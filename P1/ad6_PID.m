facf462b925e32a2a94cdfeecb91c401066be5c1
Piotr Hondra <piotr.hondra@gmail.com>
2021-11-15 21:39:57 +0100

    added writematrix, changed loss calc

--- P1/ad6_PID.m
+++ P1/ad6_PID.m
@@ -14,7 +14,7 @@ Td = params(3);
 
 SIM_LENGHT = 500;
 y_zad = zeros(SIM_LENGHT*2,1);
-y_zad(200:SIM_LENGHT*2) = 1.5;
+y_zad(200:500) = 1.5;
 y_zad(500:800) = 2.5;
 y_zad(800:1000) = 2;
 
@@ -22,14 +22,21 @@ controller = PID(K, Ti, Td, 0.5, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
 obj = Obj_15Y_p1();
 [~, u,y] = systemSim(controller, obj, y_zad, 0.5, SIM_LENGHT+0.5);
 
-fprintf("Wynik optymalizacji:\n\tK: %0.3f\n\tTi: %0.3f\n\tTd: %0.3f\n\tloss: %0.4f\n", K, Ti, Td, norm(y_zad-y));
+fprintf("Wynik optymalizacji:\n\tK: %0.3f\n\tTi: %0.3f\n\tTd: %0.3f\n\tloss: %0.4f\n", K, Ti, Td, norm(y_zad(200:end)-y(200:end)));
 figure()
-stairs(u)
+u_fig = stairs(u)
 figure()
 hold on
-stairs(y_zad)
-stairs(y)
+y_zad_fig = stairs(y_zad)
+y_fig = stairs(y)
 hold off
+
+figs = [u_fig y_zad_fig y_fig];
+fig_names = ["u","y_zad","y_fig"];
+for i = 1:size(a,2)
+    writematrix([figs(i).XData; figs(i).YData]', "p1_zadanie6_PID"+fig_names(i)+".txt", 'Delimiter','tab')
+end
+
 
 function loss = f(params)
     global MV_MIN MV_MAX dMV_MIN dMV_MAX;
