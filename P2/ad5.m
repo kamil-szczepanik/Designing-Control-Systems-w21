0463cdcb9dbce35d0e7930162607f2f3eb5dc6fb
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-11-27 18:56:01 +0100

    Zapisz również pełne odpowiedzi skokowe

--- P2/ad5.m
+++ P2/ad5.m
@@ -1,48 +1,35 @@
 clear;
-close;
-load("model.mat");
-%Kkrytyczne = 0.975
-%T = 55
+close all;
+load("data/S_u.mat");
+load("data/S_z.mat");
 SIM_LENGHT = 500;
-deltaUmax = 0.25;
-lambda = 0.001;
-N = 50;
-Nu = 50;
-MV_MIN = 0.9;
-MV_MAX = 1.5;
-dMV_MIN = -0.25;
-dMV_MAX = 0.25;
-
 
 y_zad = zeros(SIM_LENGHT*2,1);
 y_zad(200:SIM_LENGHT*2) = 1.5;
 y_zad(500:800) = 2.5;
 y_zad(800:1000) = 2;
+z_zad = 0;
 
+D = size(S_u,1);
+lambda = 1;
+N = D;
+Nu = D;
+MV_MIN = -Inf;
+MV_MAX = Inf;
+dMV_MIN = -Inf;
+dMV_MAX = Inf;
 
-controller = DMC(s, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
-obj = Obj_15Y_p1();
-[~,U_dmc,Y_dmc] = systemSim(controller, obj, y_zad, 0.5, SIM_LENGHT+0.5);
-
-controller = PID(0.585, 27.5, 6.6, 0.5, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
-obj = Obj_15Y_p1();
-[~,U_pid,Y_pid] = systemSim(controller, obj, y_zad, 0.5, SIM_LENGHT+0.5);
-
-figure()
-hold on
-stairs(y_zad)
-stairs(Y_dmc)
-hold off
-figure()
-stairs(U_dmc)
+controller = DMCz(S_u,S_z, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
+obj = Obj_15Y_p2();
+[~,U,Y] = systemSimZ(controller, obj, y_zad,z_zad, 0.5, SIM_LENGHT+0.5);
 
 figure()
 hold on
 stairs(y_zad)
-stairs(Y_pid)
+stairs(Y)
 hold off
 figure()
-stairs(U_pid)
+stairs(U)
 
-err = norm(y_zad-Y_dmc);
+err = norm(y_zad-Y);
 disp(err);
