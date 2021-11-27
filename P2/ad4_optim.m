d84ae6de36979cd7a13eefd2d4029f01af76fc92
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-11-27 19:39:13 +0100

    ad4 optim with ga

--- P2/ad4_optim.m
+++ P2/ad4_optim.m
@@ -10,14 +10,22 @@ dMV_MIN = -Inf;
 dMV_MAX = Inf;
 s = S_u;
 
-options = optimoptions(@fminunc,'MaxIterations',100,'MaxFunctionEvaluations',200);
-[params,loss] = fminunc(@f,[205,50,10,1],options);
+% options = optimoptions(@fminunc,'MaxIterations',100,'MaxFunctionEvaluations',200);
+% [params,loss] = fminunc(@f,[205,50,10,1],options);
+options = optimoptions(@ga,'MaxGenerations',80,'MaxStallGenerations',10,'PopulationSize', 10);
+params = ga(@f,4,[-1 1 0 0;0 -1 1 0],[0,0],[],[],[1,1,1,0],[470,470,470,Inf],[],[1,2,3],options);
 
 D = params(1);
 N = max(params(2),D);
 Nu = max(params(3),N);
 lambda = params(4);
 
+
+SIM_LENGHT = 500;
+y_zad = zeros(SIM_LENGHT*2,1);
+y_zad(200:SIM_LENGHT*2) = 1.5;
+y_zad(500:800) = 2.5;
+y_zad(800:1000) = 2;
 controller = DMCz(s(1:D),S_z, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
 obj = Obj_15Y_p2();
 [~, u,y] = systemSimZ(controller, obj, y_zad, 0, 0.5, SIM_LENGHT+0.5);
@@ -29,7 +37,7 @@ hold off
 figure()
 u_fig = stairs(u)
 
-fprintf("Wynik optymalizacji:\n\tN: %0.3f\n\tNu: %0.3f\n\tN: %0.3f\n\tlambda: %0.3f\n\tloss: %0.4f\n", N, Nu, D,lambda, norm(y_zad(200:end)-y(200:end)));
+fprintf("Wynik optymalizacji:\n\tN: %0.3f\n\tNu: %0.3f\n\tD: %0.3f\n\tlambda: %0.3f\n\tloss: %0.4f\n", N, Nu, D,lambda, norm(y_zad(200:end)-y(200:end)));
 
 figs = [u_fig, y_zad_fig y_fig];
 fig_names = ["u" "y_zad" "y"];
@@ -46,11 +54,13 @@ function loss = f(params)
     y_zad(800:1000) = 2;
     
     D = params(1);
-    N = max(params(2),D);
-    Nu = max(params(3),N);
+    N = params(2);
+    Nu = params(3);
+%     N = max(params(2),D);
+%     Nu = max(params(3),N);
     lambda = params(4);
     
-    controller = DMCz(s(1:D+1),S_z, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
+    controller = DMCz(s(1:D),S_z, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
     obj = Obj_15Y_p2();
     [~, ~,y] = systemSimZ(controller, obj, y_zad, 0, 0.5, SIM_LENGHT+0.5);
     loss = norm(y_zad-y);
