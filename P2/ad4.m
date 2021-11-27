8ff0cf22e1e83055ab8fc0eeb4365dc52836c88c
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-11-27 18:16:30 +0100

    Ad2 txt

--- P2/ad4.m
+++ P2/ad4.m
@@ -8,10 +8,17 @@ y_zad(200:SIM_LENGHT*2) = 1.5;
 y_zad(500:800) = 2.5;
 y_zad(800:1000) = 2;
 
+lambda
+N
+Nu
+MV_MIN = -Inf;
+MV_MAX = Inf;
+dMV_MIN = -Inf;
+dMV_MAX = Inf;
 
-controller = DMC(s, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
+controller = DMCz(S_u, lambda, N, Nu, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
 obj = Obj_15Y_p1();
-[~,U,Y] = systemSim(controller, obj, y_zad, 0.5, SIM_LENGHT+0.5);
+[~,U,Y] = systemSimZ(controller, obj, y_zad, 0.5, SIM_LENGHT+0.5);
 
 figure()
 hold on
