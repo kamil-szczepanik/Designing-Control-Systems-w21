f0bbb65e0baebc1f8ada00b17fac35acc5620475
Piotr Hondra <piotr.hondra@gmail.com>
2022-01-27 17:23:01 +0100

    added low and hight freq and noise

--- Prezentacja/PID_sim.m
+++ Prezentacja/PID_sim.m
@@ -5,14 +5,16 @@ K_pid=2.3487; Ti_pid=4.9611; Td_pid=1.06; T=1; MV_MIN=-1; MV_MAX=1; dMV_MIN=-2; 
 
 controller = PID(K_pid, Ti_pid, Td_pid, T, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
 
-K=1; Td=0; T1=5; T2=1; Y0=0; freq_noise=0;
-obj = double_inertial(K, Td, T1, T2, Y0, freq_noise);
+K=1; Td=0; T1=5; T2=1; Y0=0; freq_noise=-1; % freq_noise=-1 => wy³±czono zak³ócenia
+SIM_TIME = 600;
+
+obj = double_inertial(K, Td, T1, T2, Y0, SIM_TIME/freq_noise);
 
 val = 0.9;
-y_zad = ones(600,1)*val;
+y_zad = ones(SIM_TIME,1)*val;
 
-white_noise = false;
-[~, u,y] = systemSim(controller, obj, y_zad, 1, 600, white_noise); % @(x)1
+white_noise = true;
+[~, u,y] = systemSim(controller, obj, y_zad, 1, SIM_TIME, white_noise); % @(x)1
 disp(norm(y_zad-y));
 
 figure(2)
--- Prezentacja/main_high_freq.m
+++ Prezentacja/main_high_freq.m
@@ -1,10 +1,12 @@
 close all;
 clear;
 clc;
-global T MV_MIN MV_MAX dMV_MIN dMV_MAX K Td T1 T2 Y0 freq_noise white_noise;
+global T MV_MIN MV_MAX dMV_MIN dMV_MAX K Td T1 T2 Y0 freq_noise white_noise SIM_TIME;
 T=1; MV_MIN=-1; MV_MAX=1; dMV_MIN=-2; dMV_MAX=2;
-K=1; Td=0; T1=5; T2=1; Y0=0; freq_noise=0;
+K=1; Td=0; T1=5; T2=1; Y0=0;
+freq_noise=-1;
 white_noise = false;
+SIM_TIME = 600;
 
 options = optimoptions(@ga,'MaxGenerations',800,'MaxStallGenerations',10,'PopulationSize', 100);
 params = ga(@f,3,[],[],[],[],[0.000001,0.000001,0.000001],[20,20,20],[],options);
@@ -35,27 +37,27 @@ hold off
 
 
 function loss = f(params)
-    global T MV_MIN MV_MAX dMV_MIN dMV_MAX K Td T1 T2 Y0 freq_noise white_noise;
+    global T MV_MIN MV_MAX dMV_MIN dMV_MAX K Td T1 T2 Y0 freq_noise white_noise SIM_TIME;
     K_pid=params(1);
     Ti_pid=params(2);
     Td_pid=params(3);
 
     controller1 = PID(K_pid, Ti_pid, Td_pid, T, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
     obj1 = double_inertial(K, Td, T1, T2, Y0, freq_noise);
-    y_zad = ones(600,1)*0.9;
-    [~, u,y] = systemSim(controller1, obj1, y_zad, 1, 600, white_noise); % @(x)1
+    y_zad = ones(SIM_TIME,1)*0.9;
+    [~, u,y] = systemSim(controller1, obj1, y_zad, 1, SIM_TIME, white_noise); % @(x)1
     loss1 = norm(y_zad-y);
     
     controller2 = PID(K_pid, Ti_pid, Td_pid, T, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
     obj2 = double_inertial(K, Td, T1, T2, Y0, freq_noise);
-    y_zad = ones(600,1)*0.5;
-    [~, u,y] = systemSim(controller2, obj2, y_zad, 1, 600, white_noise); % @(x)1
+    y_zad = ones(SIM_TIME,1)*0.5;
+    [~, u,y] = systemSim(controller2, obj2, y_zad, 1, SIM_TIME, white_noise); % @(x)1
     loss2 = norm(y_zad-y);
     
     controller3 = PID(K_pid, Ti_pid, Td_pid, T, MV_MIN, MV_MAX, dMV_MIN, dMV_MAX);
     obj3 = double_inertial(K, Td, T1, T2, Y0, freq_noise);
-    y_zad = ones(600,1)*0.2;
-    [~, u,y] = systemSim(controller3, obj3, y_zad, 1, 600, white_noise); % @(x)1
+    y_zad = ones(SIM_TIME,1)*0.2;
+    [~, u,y] = systemSim(controller3, obj3, y_zad, 1, SIM_TIME, white_noise); % @(x)1
     loss3 = norm(y_zad-y);
     
     loss = loss1 + loss2 + loss3
